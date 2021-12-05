2b0a752421accea9042791e07cd6dc2db31135ca
robertdeg <robertonl@gmail.com>
2021-12-05 14:57:51 +0100

    Added solution to day 5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import re
 from functools import partial
 from aoc.week1 import *
-from itertools import groupby
+from itertools import groupby, chain
 
 def build_board(numbers: list) -> (int, dict):
     return len(numbers), {int(value) : (r, c) for r, row in enumerate(numbers) for c, value in enumerate(row)}
@@ -45,15 +46,45 @@ def day4(lines):
             scores.append(score_board(board, drawn, dim))
 
     scores = list(sorted(scores))
-    print("Day 5 part 1: {}".format(scores[0][1]))
-    print("Day 5 part 2: {}".format(scores[-1][1]))
+    print("Day 4 part 1: {}".format(scores[0][1]))
+    print("Day 4 part 2: {}".format(scores[-1][1]))
+
+def is_diagonal(x1, y1, x2, y2):
+    return x1 != x2 and y1 != y2
+
+def points(x1, y1, x2, y2):
+    if x1 == x2:
+        return ((x1, y) for y in range(min([y1, y2]), max([y1, y2]) + 1))
+    elif y1 == y2:
+        return ((x, y1) for x in range(min([x1, x2]), max([x1, x2]) + 1))
+    else:
+        mx = 1 if x2 > x1 else -1
+        my = 1 if y2 > y1 else -1
+        return ((x1 + mx * delta, y1 + my * delta) for delta in range(abs(x1 - x2) + 1))
+
+def day5(lines):
+    matches = (re.match("(\d+),(\d+) -> (\d+),(\d+)", line) for line in lines)
+    all_lines = [(int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4))) for m in matches]
+    straight_lines = list(filter(lambda tuple: not is_diagonal(*tuple), all_lines))
+
+    points_on_straight_lines = sorted(list(chain.from_iterable(points(*xs) for xs in straight_lines)))
+    overlaps_straight_lines = (len(list(g)) for point, g in groupby(points_on_straight_lines))
+
+    points_on_all_lines = sorted(list(chain.from_iterable(points(*xs) for xs in all_lines)))
+    overlap_all_lines = (len(list(g)) for point, g in groupby(points_on_all_lines))
+
+    at_least_two = lambda x : x > 1
+
+    print("Day 5 part 1: {}".format(sum(1 for x in filter(at_least_two, overlaps_straight_lines))))
+    print("Day 5 part 2: {}".format(sum(1 for x in filter(at_least_two, overlap_all_lines))))
 
 if __name__ == '__main__':
     d = {
         1: [partial(day1, 1), partial(day1, 3)],
         2: [day2p1, day2p2],
         3: day3,
-        4: day4}
+        4: day4,
+        5: day5}
 
     for num, funs in d.items():
         try:
