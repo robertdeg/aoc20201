ee738f6a5a98628a6e5a4e211322fcbe255ac1a1
robertdeg <robertonl@gmail.com>
2021-12-12 11:36:12 +0100

    Solved 11 & 12

--- main.py
+++ main.py
@@ -30,10 +30,84 @@ def day10(lines):
     print("Day 10 part 1: {}".format(sum(s for s, stack in scores)))
     print("Day 10 part 2: {}".format(ss[len(ss) // 2]))
 
+def day11(lines):
+    map = {(int(row), int(col)): int(value) for row, values in enumerate(lines) for col, value in enumerate(values)}
+    dims_x = min(x for x, y in map), max(x for x, y in map) + 1
+    dims_y = min(y for x, y in map), max(y for x, y in map) + 1
+    def adjacent(x, y):
+        return (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x-1,y-1), (x+1,y+1), (x-1,y+1), (x+1,y-1)
+
+    def step(values: dict, flashes: int) -> (dict, int):
+        result = dict()
+        for pos in values:
+            result[pos] = values[pos] + 1
+
+        n = 1
+        while n > 0:
+            n = 0
+            for pos, value in result.items():
+                if value > 9:
+                    n += 1
+                    result[pos] = -1
+                    for nbor in adjacent(*pos):
+                        if result.get(nbor, -1) > -1:
+                            result[nbor] += 1
+
+            flashes += n
+
+        for pos, value in result.items(): result[pos] = 0 if value < 0 else value
+
+        return result, flashes
+
+    def print_map(values: dict):
+        for y in range(*dims_y):
+            for x in range(*dims_x):
+                value = values[(y, x)]
+                if value > 0:
+                    print(value, end='')
+                else:
+                    print(' ', end='')
+            print()
+        print()
+
+    flashes = 0
+    steps = 0
+    print_map(map)
+    for _ in range(1000):
+        map, flashes = step(map, flashes)
+        if len({v for v in map.values()}) == 1:
+            break
+
+        steps += 1
+        # print_map(map)
+
+    print("Day 11 part 1: {}".format(flashes))
+    print("Day 11 part 2: {}".format(steps + 1))
+
+def day12(lines):
+    edges = list(line.split("-") for line in lines)
+    outgoing = {src : list(map(operator.itemgetter(1), g)) for src, g in groupby(sorted(edges, key = operator.itemgetter(0)), key = operator.itemgetter(0))}
+    incoming = {src : list(map(operator.itemgetter(0), g)) for src, g in groupby(sorted(edges, key = operator.itemgetter(1)), key = operator.itemgetter(1))}
+
+    def num_paths(v: str, visited: dict) -> int:
+        if v == "end": return 1
+        n = 0
+        visited = dict(visited)
+        if v.islower(): visited[v] = visited.get(v, 0) + 1
+        for w in chain(iter(incoming.get(v, [])), iter(outgoing.get(v, []))):
+            if w.isupper():
+                n += num_paths(w, visited)
+            elif w not in visited or w not in ("start", "end") and sum(1 for _, k in visited.items() if k > 1) == 0:
+                n += num_paths(w, visited)
+        return n
+
+    print("Day 12 part 1: {}".format(num_paths("start", dict())))
+
 if __name__ == '__main__':
     d = {
-        1: [partial(day1, 1), partial(day1, 3)], 2: [day2p1, day2p2], 3: day3, 4: day4, 5: day5, 6: day6, 7: day7,
-        8: day8, 9: day9, 10: day10}
+        #1: [partial(day1, 1), partial(day1, 3)], 2: [day2p1, day2p2], 3: day3, 4: day4, 5: day5, 6: day6, 7: day7,
+        #8: day8, 9: day9, 10: day10, 11: day11,
+        12: day12}
 
     for num, funs in d.items():
         try:
@@ -43,5 +117,5 @@ if __name__ == '__main__':
 
         for fun in funs:
             with open("input/day{}.txt".format(num)) as file:
-                lines = (line.strip() for line in file.readlines())
+                lines = [line.strip() for line in file.readlines()]
                 fun(lines)
