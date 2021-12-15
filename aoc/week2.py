430dfbbee41d20529e0512b35a4e996d4df561af
robertdeg <robertonl@gmail.com>
2021-12-15 15:27:50 +0100

    Solved day 15

--- aoc/week2.py
+++ aoc/week2.py
@@ -149,3 +149,68 @@ def day12(lines):
     print("Day 12 part 1: {}".format(num_paths("start", dict())))
     print("Day 12 part 2: {}".format(num_paths("start", dict(), 1)))
 
+def day13(lines):
+    grouped = map(operator.itemgetter(1), groupby(lines, lambda line : len(line) > 0))
+    coordinates = {(int(x), int(y)) for x, y in map(lambda line: line.split(","), next(grouped))}
+    next(grouped)
+    instructions = map(lambda line: re.match(r"fold along ([x|y])=(\d+)", line).groups(), next(grouped))
+
+    def fold(axis, index, cs):
+        if axis == "x":
+            return {(2 * int(index) - x if x > int(index) else x, y) for x, y in cs}
+        else:
+            return {(x, 2 * int(index) - y if y > int(index) else y) for x, y in cs}
+
+    coordinates = fold(*next(instructions), coordinates)
+    print("Day 13 part 1: {}".format(len(coordinates)))
+
+    coordinates = reduce(lambda cs, instr: fold(*instr, cs), instructions, coordinates)
+
+    max_x = max(map(operator.itemgetter(0), coordinates))
+    max_y = max(map(operator.itemgetter(1), coordinates))
+
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            print("{}".format("@@" if (x, y) in coordinates else "  "), end="")
+        print()
+
+def day14(lines):
+    template = next(lines)
+    next(lines)
+    instructions = (re.match(r"([A-Z][A-Z]) -> ([A-Z])", line).groups() for line in lines)
+    rules = {pair : insert for pair, insert in instructions}
+    pairs = Counter(a + b for a, b in zip(template, template[1:]))
+
+    def step(counts: Counter) -> Counter:
+        result = Counter()
+        for pair, count in counts.items():
+            insert = rules.get(pair, None)
+            if insert:
+                result[pair[0] + insert] += count
+                result[insert + pair[1]] += count
+            else:
+                result[pair] += count
+
+        return result
+
+    def score(pairs: Counter) -> Counter:
+        charcounts = Counter({template[0]: -1, template[-1]: -1})
+        for (a, b), count in pairs.items():
+            charcounts[a] += count
+            charcounts[b] += count
+
+        charcounts = Counter({c: n // 2 for c, n in charcounts.items()}) + Counter({template[0]: 1, template[-1]: 1})
+        min_count = min(charcounts.values())
+        max_count = max(charcounts.values())
+        return max_count - min_count
+
+    for _ in range(10):
+        pairs = step(pairs)
+
+    print("Day 14 part 1: {}".format(score(pairs)))
+
+    for _ in range(30):
+        pairs = step(pairs)
+
+    print("Day 14 part 2: {}".format(score(pairs)))
+
--- main.py
+++ main.py
@@ -6,77 +6,50 @@ from aoc.week2 import *
 from itertools import groupby, chain, dropwhile, tee
 from collections import Counter
 from functools import reduce
+import heapq as hq
 
-def day13(lines):
-    grouped = map(operator.itemgetter(1), groupby(lines, lambda line : len(line) > 0))
-    coordinates = {(int(x), int(y)) for x, y in map(lambda line: line.split(","), next(grouped))}
-    next(grouped)
-    instructions = map(lambda line: re.match(r"fold along ([x|y])=(\d+)", line).groups(), next(grouped))
+def day15(lines):
+    def adjacent(height: int, width: int, row: int, col: int):
+        if row < height - 1: yield row + 1, col
+        if col < width - 1: yield row, col + 1
+        if row > 0: yield row - 1, col
+        if col > 0: yield row, col - 1
 
-    def fold(axis, index, cs):
-        if axis == "x":
-            return {(2 * int(index) - x if x > int(index) else x, y) for x, y in cs}
-        else:
-            return {(x, 2 * int(index) - y if y > int(index) else y) for x, y in cs}
+    risks = {(int(row), int(col)): int(risk) for row, cols in enumerate(lines) for col, risk in enumerate(cols)}
+    height, width = map(operator.add, reduce(lambda tup1, tup2 : map(max, tup1, tup2), risks), (1, 1))
 
-    coordinates = fold(*next(instructions), coordinates)
-    print("Day 13 part 1: {}".format(len(coordinates)))
+    def risk(row: int, col: int) -> int:
+        return (risks[(row % height, col % width)] + row // height + col // width - 1) % 9 + 1;
 
-    coordinates = reduce(lambda cs, instr: fold(*instr, cs), instructions, coordinates)
+    def distances(height: int, width: int, row: int, col: int):
+        visited = set()
+        shortest = {(0, 0): 0}
+        queue = [(0, (0, 0))]
+        while queue:
+            dist, pos = hq.heappop(queue)
+            if pos in visited:
+                continue
 
-    max_x = max(map(operator.itemgetter(0), coordinates))
-    max_y = max(map(operator.itemgetter(1), coordinates))
+            if pos == (row, col):
+                return dist
 
-    for y in range(max_y + 1):
-        for x in range(max_x + 1):
-            print("{}".format("#" if (x, y) in coordinates else " "), end="")
-        print()
+            visited.add(pos)
+            for adj in adjacent(height, width, *pos):
+                if adj not in shortest or dist + risk(*adj) < shortest[adj]:
+                    shortest[adj] = dist + risk(*adj)
+                    hq.heappush(queue, (shortest[adj], adj))
 
-def day14(lines):
-    template = next(lines)
-    next(lines)
-    instructions = (re.match(r"([A-Z][A-Z]) -> ([A-Z])", line).groups() for line in lines)
-    rules = {pair : insert for pair, insert in instructions}
-    pairs = Counter(a + b for a, b in zip(template, template[1:]))
+        return None
 
-    def step(counts: Counter) -> Counter:
-        result = Counter()
-        for pair, count in counts.items():
-            insert = rules.get(pair, None)
-            if insert:
-                result[pair[0] + insert] += count
-                result[insert + pair[1]] += count
-            else:
-                result[pair] += count
+    print("Day 15 part 1: {}".format(distances(height, width, height - 1, width - 1)))
+    print("Day 15 part 2: {}".format(distances(height * 5, width * 5, height * 5 - 1, width * 5 - 1)))
 
-        return result
-
-    def score(pairs: Counter) -> Counter:
-        charcounts = Counter({template[0]: -1, template[-1]: -1})
-        for (a, b), count in pairs.items():
-            charcounts[a] += count
-            charcounts[b] += count
-
-        charcounts = Counter({c: n // 2 for c, n in charcounts.items()}) + Counter({template[0]: 1, template[-1]: 1})
-        min_count = min(charcounts.values())
-        max_count = max(charcounts.values())
-        return max_count - min_count
-
-    for _ in range(10):
-        pairs = step(pairs)
-
-    print("Day 14 part 1: {}".format(score(pairs)))
-
-    for _ in range(30):
-        pairs = step(pairs)
-
-    print("Day 14 part 2: {}".format(score(pairs)))
 
 if __name__ == '__main__':
     d = {
         1: [partial(day1, 1), partial(day1, 3)], 2: [day2p1, day2p2], 3: day3, 4: day4, 5: day5, 6: day6, 7: day7,
-        8: day8, 9: day9, 10: day10, 11: day11, 12: day12, 13: day13,
-        14: day14}
+        8: day8, 9: day9, 10: day10, 11: day11, 12: day12, 13: day13, 14: day14,
+        15: day15}
 
     for num, funs in d.items():
         try:
